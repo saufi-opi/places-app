e1f91d30b32b7240f8e828e617a1eb0895e86643
Saufi <saufi1115@gmail.com>
2024-05-26 02:35:10 +0800

    feat: explore page

---
+++ src/app/(home)/explore/page.tsx
@@ -0,0 +1,28 @@
+import MaxWidthWrapper from '@/components/global/max-width-wrapper'
+import Search from '@/components/global/filter-components/search'
+import PlacesFilter from '@/components/places/places-filter'
+import PlacesList from '@/components/places/places-list'
+import PlacesListSkeleton from '@/components/places/places-list-skeleton'
+import React, { Suspense } from 'react'
+
+function ExplorePage() {
+  return (
+    <MaxWidthWrapper className="my-20 flex flex-col gap-5 lg:flex-row">
+      <div className="space-y-10 lg:w-1/3">
+        <h2 className="text-xl font-bold">Filter Places</h2>
+        <PlacesFilter />
+      </div>
+      <div className="space-y-10">
+        <div className="flex flex-col justify-between gap-5 md:flex-row">
+          <h1 className="text-3xl font-bold">Discover Places</h1>
+          <Search />
+        </div>
+        <Suspense fallback={<PlacesListSkeleton />}>
+          <PlacesList />
+        </Suspense>
+      </div>
+    </MaxWidthWrapper>
+  )
+}
+
+export default ExplorePage
