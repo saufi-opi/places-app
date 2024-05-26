e1bd981c362c28f943288793bb748a8d076def4c
Saufi <saufi1115@gmail.com>
2024-05-27 00:12:28 +0800

    feat: slugify function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,11 @@
 import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
+import createSlug from 'slugify'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function slugify(input: string) {
+  return createSlug(input, { lower: true, strict: true, locale: 'en' })
 }
