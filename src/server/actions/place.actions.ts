7f11b754a2a3cc3d070b874a39c87780c82c1369
Saufi <saufi1115@gmail.com>
2024-05-27 00:13:04 +0800

    feat: addPlace action

---
+++ src/server/actions/place.actions.ts
@@ -0,0 +1,32 @@
+'use server'
+
+import { db } from '../db'
+import zod from 'zod'
+import { revalidatePath } from 'next/cache'
+
+export const PlaceZodSchema = zod.object({
+  title: zod.string(),
+  description: zod.string(),
+  thumbnail: zod.string(),
+  address: zod.string(),
+  categorySlug: zod.string(),
+  googleMap: zod.string(),
+  submitBy: zod.string().optional()
+})
+
+export const addPlace = async (formData: FormData) => {
+  const parsed = PlaceZodSchema.safeParse(formData)
+  if (!parsed.success) {
+    return {
+      success: false,
+      message: parsed.error.message
+    }
+  }
+  const item = await db.place.create({ data: parsed.data })
+  revalidatePath('/explore')
+
+  return {
+    success: true,
+    item
+  }
+}
