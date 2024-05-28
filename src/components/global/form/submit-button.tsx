c276f64cfe40a8c5b5985c869dd23a8289dcf3e2
saufi <saufi1115@gmail.com>
2024-05-28 15:23:37 +0800

    feat: classname prop

--- src/components/global/form/submit-button.tsx
+++ src/components/global/form/submit-button.tsx
@@ -1,15 +1,16 @@
 'use client'
 
 import { Button } from '@/components/ui/button'
+import { cn } from '@/lib/utils'
 import { LoaderIcon } from 'lucide-react'
 import React from 'react'
 import { useFormStatus } from 'react-dom'
 
-function SubmitButton({ title }: { title: string }) {
+function SubmitButton({ title, className }: { title: string; className?: string }) {
   const { pending } = useFormStatus()
 
   return (
-    <Button type="submit" disabled={pending}>
+    <Button className={cn('', className)} type="submit" disabled={pending}>
       {pending ? <LoaderIcon className="h-6 w-6 animate-spin" /> : title}
     </Button>
   )
