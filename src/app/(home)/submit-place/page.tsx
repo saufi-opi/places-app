8cf9bef0719fb056eb5a42b2e81a359a964500c2
saufi <saufi1115@gmail.com>
2024-05-28 14:45:37 +0800

    feat: submit a place page

---
+++ src/app/(home)/submit-place/page.tsx
@@ -0,0 +1,12 @@
+import MaxWidthWrapper from '@/components/global/max-width-wrapper'
+import React from 'react'
+
+function SubmitPlace() {
+  return (
+    <MaxWidthWrapper>
+      <h1>Submit A Place</h1>
+    </MaxWidthWrapper>
+  )
+}
+
+export default SubmitPlace
