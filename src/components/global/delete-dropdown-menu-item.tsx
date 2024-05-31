b57133f442c2b6956959b2e711648d7ca5b7ea80
saufi <saufi1115@gmail.com>
2024-06-01 02:04:49 +0800

    feat: deleteDropdown item

---
+++ src/components/global/delete-dropdown-menu-item.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import React from 'react'
+import { DropdownMenuItem } from '../ui/dropdown-menu'
+
+interface Props {
+  id: string
+  action: unknown
+}
+
+function DeleteDropDownMenuItem(props: Props) {
+  const handleClick = async () => {
+    if (typeof props.action === 'function') {
+      await props.action(props.id)
+    }
+  }
+
+  return (
+    <DropdownMenuItem className="text-destructive" onClick={handleClick}>
+      Delete
+    </DropdownMenuItem>
+  )
+}
+
+export default DeleteDropDownMenuItem
