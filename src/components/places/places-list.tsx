54238f9b629986081108463e59d90e05c0fa3c2a
saufi <saufi1115@gmail.com>
2024-06-05 11:30:46 +0800

    refactor: render pagination although no result -> for reset page number to base on total page

--- src/components/places/places-list.tsx
+++ src/components/places/places-list.tsx
@@ -20,16 +20,10 @@ async function PlacesList({ searchParams }: Props) {
   // Fetch data and count from the database
   const { data, totalPages } = await getPlaces({ page, pageSize, categories, search })
 
-  if (data.length === 0) {
-    return <h2 className="text-xl font-bold text-zinc-400">0 places found...</h2>
-  }
-
   return (
     <>
       <div className="grid grid-cols-1 gap-x-5 gap-y-10 md:grid-cols-2 lg:grid-cols-3">
-        {data.map((d) => (
-          <PlaceCard key={d.id} place={d} />
-        ))}
+        {data.length ? data.map((d) => <PlaceCard key={d.id} place={d} />) : <h2 className="text-xl font-bold text-zinc-400">0 places found...</h2>}
       </div>
       <MyPagination page={page} totalPages={totalPages} />
     </>
